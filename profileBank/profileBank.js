9191a54fef4cc6afa5fe8b649b8a94090bc8c5f0
Aarti Shelukar <aarti.shelukar@Eternusdev.local>
2016-05-18 19:15:56 +0530

    Changes in Profile Bank API

--- profileBank/profileBank.js
+++ profileBank/profileBank.js
@@ -55,6 +55,22 @@ var getQualificationDetails = function (req, res) {
     res.json(profileBank.candidateProfile[index].Qualifications);
 };
 
+var updateQualifications = function (req, res) {
+    var qualification = req.body.qualification;
+    var index = _.findIndex(profileBank.candidateProfile, { CandidateID: qualification.CandidateID });
+    var QIndex = _.findIndex(profileBank.candidateProfile[index].Qualifications , {QualificationID: qualification.QualificationID } );
+    profileBank.candidateProfile[index].Qualifications[QIndex] = qualification;
+    res.json(qualification);
+};
+
+var updateCandidateProfileStatus = function (req, res) {
+    var profile = req.body.profile;
+    var index = _.findIndex(profileBank.candidateProfile, { CandidateID: profile.CandidateID });
+    profileBank.candidateProfile[index].Status[0] = profile.Status;
+    profileBank.candidateProfile[index].Comments = profile.Comments;
+    res.json(profile);
+};
+
 module.exports = function (app) {
     app.get('/api/ProfileBank/getOpenProfiles', utils.EnsureAuthenticated, getOpenProfiles);
     app.post('/api/ProfileBank/addCandidateProfile', utils.EnsureAuthenticated, addCandidateProfile);
@@ -70,9 +86,10 @@ module.exports = function (app) {
     app.post('/api/ProfileBank/AddCandidateSkillsDetails', utils.EnsureAuthenticated, editCandidateProfile);
     app.post('/api/ProfileBank/AddCandidateSalaryDetails', utils.EnsureAuthenticated, editCandidateProfile);
     app.post('/api/ProfileBank/AddCandidateTeamManagementDetails', utils.EnsureAuthenticated, editCandidateProfile);
-    app.post('/api/ProfileBank/UpdateStatus', utils.EnsureAuthenticated, editCandidateProfile);
+    app.post('/api/ProfileBank/UpdateStatus', utils.EnsureAuthenticated, updateCandidateProfileStatus);
     app.post('/api/ProfileBank/AddQualificationDetails', utils.EnsureAuthenticated, AddQualificationDetails);
     app.post('/api/ProfileBank/getQualificationDetails', utils.EnsureAuthenticated, getQualificationDetails);
 
+    app.post('/api/Masters/UpdateQualifications', utils.EnsureAuthenticated, updateQualifications);
 
 };
