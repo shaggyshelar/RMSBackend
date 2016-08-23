20ea43b3c411b7105239af71c61568f691713977
SagarShelar <sagar.shelar@eternussolutions.com>
2016-08-23 14:27:35 +0530

    server.js changes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
