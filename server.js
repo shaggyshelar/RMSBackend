3f9ccf61d9b2ca475d8a86b99c5c9050f76244e5
SagarShelar <sagar.shelar@eternussolutions.com>
2016-08-23 14:05:37 +0530

    server.js changes

--- server.js
+++ server.js
@@ -5,7 +5,21 @@ var express = require('express'),
 var path = require('path');
 var bodyParser = require('body-parser');
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 require('./auth/auth')(app);
